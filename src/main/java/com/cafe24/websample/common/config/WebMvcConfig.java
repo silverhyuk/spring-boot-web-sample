c723e6d0b7d77ae8246872c2e7b254c8aa5c16f5
ehchoi <ehchoi@cafe24corp.com>
2018-12-17 10:36:41 +0900

    소스정리

--- src/main/java/com/cafe24/websample/common/config/WebMvcConfig.java
+++ src/main/java/com/cafe24/websample/common/config/WebMvcConfig.java
@@ -1,13 +1,11 @@
 package com.cafe24.websample.common.config;
 
 import com.cafe24.websample.common.interceptor.CommonInterceptor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
