6aeddcb88258ed71ca8fe11b965d741152da7137
ehchoi <ehchoi@cafe24corp.com>
2018-12-17 09:37:43 +0900

    cors

--- src/main/java/com/cafe24/websample/common/config/WebMvcConfig.java
+++ src/main/java/com/cafe24/websample/common/config/WebMvcConfig.java
@@ -1,7 +1,10 @@
 package com.cafe24.websample.common.config;
 
 import com.cafe24.websample.common.interceptor.CommonInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -15,5 +18,14 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .addPathPatterns("/**/*")
                 .excludePathPatterns("/test/**/")
                 .excludePathPatterns("/login.ws"); //로그인 쪽은 예외처리를 한다.
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("*")
+                .allowedMethods(HttpMethod.POST.name())
+                .allowCredentials(false)
+                .maxAge(3600);
     }
 }
