750febc52c959ea3a7529f7be5ab157d9bb2e59a
silverhyuk84 <silverhyuk84@gmail.com>
2019-01-13 21:22:49 +0900

    decorate 변경

--- src/main/java/com/cafe24/websample/common/aspect/LoggerAspect.java
+++ src/main/java/com/cafe24/websample/common/aspect/LoggerAspect.java
@@ -7,8 +7,10 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 @Aspect
+@Component
 public class LoggerAspect {
     //protected Logger logger = LoggerFactory.getLogger(LoggerAspect.class);
     private static final Logger logger = LoggerFactory.getLogger(LoggerAspect.class);
