bcf65c7ea6a6a1471fa5b5872042544c18018f84
silverhyuk84 <silverhyuk84@gmail.com>
2019-05-11 07:03:09 +0900

    aop 추가

--- src/main/java/com/cafe24/websample/common/aspect/LoggerAspect.java
+++ src/main/java/com/cafe24/websample/common/aspect/LoggerAspect.java
@@ -5,6 +5,7 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -12,18 +13,18 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class LoggerAspect {
-    //protected Logger logger = LoggerFactory.getLogger(LoggerAspect.class);
-    private static final Logger logger = LoggerFactory.getLogger(LoggerAspect.class);
+    //protected Logger log = LoggerFactory.getLogger(LoggerAspect.class);
+    private static final Logger log = LoggerFactory.getLogger(LoggerAspect.class);
     static String name = "";
     static String type = "";
 
 	//@Before("@annotation(com.cafe24.websample.common.annotation.LogAround)")
 	public void logBefore(JoinPoint joinPoint) {
-		logger.debug("logBefore()");
+		log.debug("logBefore()");
 	}
 
 	public void logAfter(JoinPoint joinPoint) {
-		logger.debug("logAfter()");
+		log.debug("logAfter()");
 	}
 
     //@Around("execution(* com.cafe24.websample..*Control*(..)) or execution(* com.cafe24.websample..*Service*(..)) or execution(* com.cafe24.websample..*Dao*(..))")
@@ -36,25 +37,61 @@ public class LoggerAspect {
 
 		long currentTime = System.currentTimeMillis();
 
-		//if (logger.isDebugEnabled()) {
-			logger.info("=================================================");
-			logger.info("START | " + className + "|" + joinPoint.getSignature().getName());
+		//if (log.isDebugEnabled()) {
+			log.info("=================================================");
+			log.info("START | " + className + "|" + joinPoint.getSignature().getName());
 
-/*			logger.debug("[class]:" + className);
-			logger.debug("[method]:" + joinPoint.getSignature().getName() + "()");*/
+/*			log.debug("[class]:" + className);
+			log.debug("[method]:" + joinPoint.getSignature().getName() + "()");*/
 		//}
 
 		Object returnObj = joinPoint.proceed();
 		
-		//if (logger.isDebugEnabled()) {
-/*			logger.info("[class]:" + className);
-			logger.info("[method]:" + joinPoint.getSignature().getName() + "()");*/
-			logger.info("[소요시간]: {}ms", new Object[] { (System.currentTimeMillis() - currentTime) });
-			logger.info("END | " + className + "|" + joinPoint.getSignature().getName());
-			logger.info("=================================================");
+		//if (log.isDebugEnabled()) {
+/*			log.info("[class]:" + className);
+			log.info("[method]:" + joinPoint.getSignature().getName() + "()");*/
+			log.info("[소요시간]: {}ms", new Object[] { (System.currentTimeMillis() - currentTime) });
+			log.info("END | " + className + "|" + joinPoint.getSignature().getName());
+			log.info("=================================================");
 		//}
 
 		return returnObj;
 
     }
+
+	//모든 Service Class
+	@Around("execution(* com..*Service.*(..))")
+	public Object commonServiceLogging(ProceedingJoinPoint joinPoint) throws Throwable {
+
+		long currentTime = System.currentTimeMillis();
+		String className = joinPoint.getSignature().getDeclaringTypeName();
+		String methodName = joinPoint.getSignature().getName();
+
+		log.debug("=================================================");
+		log.debug(">>>>>>>>> LOGGING START >>>>>>>>>>");
+		log.debug("[class]:{}", className);
+		log.debug("[method]:{}()", methodName);
+
+		Object result = joinPoint.proceed();
+
+		//log.debug("[class]:{}", className);
+		//log.debug("[method]:{}()", methodName);
+		log.debug("[소요시간]: {}ms", new Object[]{(System.currentTimeMillis()-currentTime)});
+		log.debug(">>>>>>>>>> LOGGING END >>>>>>>>>>");
+		log.debug("=================================================");
+
+		return result;
+	}
+
+	@Pointcut("execution(public * com..*Controller.*(..))")
+	public void controllerClassMethods() {}
+
+	@Before(value = "controllerClassMethods()")
+	public void checkSessionValid(JoinPoint joinPoint) {
+		String token = joinPoint.getSignature().getDeclaringTypeName(); //JoinPoint클래스를 이용하여 타켓 메서드의 파라미터를 가로챌 수 있다.
+		log.debug("Controller Before AOP >>>>>>>>>>>>>>>  {}", token);
+	}
+
+
+
 }
